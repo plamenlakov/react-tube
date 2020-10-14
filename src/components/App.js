b5ce3f95afbb169ad3da3b0ea7544e149f2c9c07
plamenlakov <frainbreeze3@gmail.com>
2020-10-14 20:37:39 +0200

    Complete VideoList and VideoItem components

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 import youtube from "../api/youtube";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo : [] };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
-      },
+      }
     });
 
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo : video });
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos.
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
       </div>
     );
   }

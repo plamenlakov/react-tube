3d49e20f61b7c137f1795c4458916f5edee78b6c
plamenlakov <frainbreeze3@gmail.com>
2020-10-20 15:51:42 +0200

    Updates

---
+++ precache-manifest.9b2201ad31506f37c7ce4b9ad9f6a4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ea35f53d9fd727f121ced94c7bfc60",
+    "url": "/react-tube/index.html"
+  },
+  {
+    "revision": "f9d2b687eba50562dfd7",
+    "url": "/react-tube/static/css/main.4c226a34.chunk.css"
+  },
+  {
+    "revision": "286a4f4db5eca22e53c1",
+    "url": "/react-tube/static/js/2.25b3e93d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tube/static/js/2.25b3e93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d2b687eba50562dfd7",
+    "url": "/react-tube/static/js/main.f0aed01d.chunk.js"
+  },
+  {
+    "revision": "25fa0fe780bc863f2681",
+    "url": "/react-tube/static/js/runtime-main.1cad2dc8.js"
+  }
+]);
